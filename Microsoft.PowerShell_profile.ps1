8229396f640a8393a76fa13a3c016ada5aaaac1f
Adrian Borup <avborup@gmail.com>
2020-06-22 13:49:38 +0200

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,15 @@
 Set-Alias vi nvim
+
+$VIMDIR = "$HOME\Appdata\Local\nvim"
+$VIMFILE = "$VIMDIR\init.vim"
+
+function prompt {
+    # Abbreviate the path unix-style
+    $path = "$(Get-Location)".replace($HOME, "~")
+
+    # Add the path to the prompt in a different style
+    Write-Host $path -NoNewline -ForegroundColor Yellow
+
+    # Return the remaining prompt content
+    "`r`n$("+"*(Get-Location -Stack).Count)> "
+}
