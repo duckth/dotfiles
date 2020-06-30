7b87f3f3caf115b8a0ac657dc31617cddf91ccad
Adrian Borup <avborup@gmail.com>
2020-06-30 10:24:49 +0200

    Add custom gt and pd commands to PS

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -18,3 +18,44 @@ function prompt {
 function elevate {
     Start-Process "alacritty" -Verb RunAs
 }
+
+# gt: shorthand for "go to", a custom version of cd that saves the cd location.
+function gt($to) {
+    $path = Resolve-Path $to -ErrorAction Stop
+    cd $path -ErrorAction Stop
+
+    $saveAt = "$($HOME)\.custom\gthistory.txt"
+
+    # Create history file if it does not yet exist
+    If (!(Test-Path $saveAt)) {
+        New-Item -ItemType File -Force -Path $saveAt
+    }
+
+    # Limit number of history entries (will delete earliest entries)
+    $maxHistory = 10
+    If ((Get-Content $saveAt | Measure-Object -Line).Lines -ge $maxHistory) {
+        (Get-Content $saveAt | Select-Object -Skip 1) | Set-Content $saveAt
+    }
+
+    Add-Content $saveAt $path
+}
+
+# pd: shorthand for "previous directory", a function to enter a previously saved
+# directory (as saved with the gt function).
+function pd {
+    # Sets up autocomplete. All results from history that contain the first arg
+    # as a substring will be autocompleted with the tab key.
+    Param (
+        [ArgumentCompleter({
+            $inp = $args[2];
+
+            $history = "$($HOME)\.custom\gthistory.txt"
+            $paths = Get-Content $history | Sort | Get-Unique
+
+            $paths | Where-Object { $_ -like "*$inp*" }
+        })]
+        [String] $path
+    )
+
+    cd $path
+}
