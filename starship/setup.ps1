3bdd1c17db7ca356b00652737ace0b34cb31e15f
Andreas Trøstrup <andreastrstrup@gmail.com>
2021-08-14 13:18:21 +0200

    update setup scripts for consistency

--- starship/setup.ps1
+++ starship/setup.ps1
@@ -1,6 +1,6 @@
 Push-Location $HOME/.config
 try {
-    New-Item -ItemType SymbolicLink -Name starship.toml -Target C:/Users/andre/dotfiles/starship/starship.toml -ea Stop
+    New-Item -ItemType SymbolicLink -Name starship.toml -Target $HOME/dotfiles/starship/starship.toml -ea Stop
     echo 'Linked starship.toml (Starship Terminal)'
 } catch [Exception] {
     Write-Host 'Failed to link starship.toml (Starship Terminal): ' -NoNewLine
--- windows-terminal/setup.ps1
+++ windows-terminal/setup.ps1
@@ -1,6 +1,6 @@
 Push-Location $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState
 try {
-    New-Item -ItemType HardLink -Name settings.json -Target C:/Users/andre/dotfiles/windows-terminal/settings.json -ea Stop
+    New-Item -ItemType HardLink -Name settings.json -Target $HOME/dotfiles/windows-terminal/settings.json -ea Stop
     echo 'Linked settings.json (Windows Terminal Preview)'
 } catch [Exception] {
     Write-Host 'Failed to link settings.json (Windows Terminal Preview): ' -NoNewLine
