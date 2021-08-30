14cb7902d872348adb2a5cd528e4ceca91e6b38d
Andreas Trøstrup <8415722+Duckth@users.noreply.github.com>
2021-08-30 18:46:55 +0200

    ps: remove alias for direct path to execs

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -62,9 +62,6 @@ Set-Alias -Name "addpath" -Value "Update-PathEnvironmentVariable"
 Set-Alias -Name "grep" -Value "rg"
 Set-Alias -Name "vim" -Value "nvim"
 Set-Alias -Name "vi" -Value "nvim"
-# Set-Alias -name "winword" -Value "C:\Program Files\Microsoft Office 15\root\office15\winword.exe"
-Set-Alias -Name "vlc" -Value "C:\Program Files\VideoLAN\VLC\vlc.exe"
-# Set-Alias -Name "audacity" -Value "C:\Program Files (x86)\Audacity\audacity.exe"
 Set-Alias -Name "touch" -Value "New-Item"
 function ll() { lsd -l }
 
