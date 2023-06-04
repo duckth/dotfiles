28e32f40cc14970fc60788f43ce71ea13de98580
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-06-04 21:26:53 +0200

    add lazyvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "mix"
+vim.g.gruvbox_material_better_performance = 1
